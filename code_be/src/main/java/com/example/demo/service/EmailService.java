34cc4118a46fa68ce9133b80ec5d658647585b97
Nomishi <tucomoemoe@gmail.com>
2026-04-14 00:27:54 +0700

    Update EmailService.java

--- code_be/src/main/java/com/example/demo/service/EmailService.java
+++ code_be/src/main/java/com/example/demo/service/EmailService.java
@@ -1,9 +1,8 @@
 package com.example.demo.service;
 
 import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
